8229f8d9397854def6ae7d1489ff4508e518a4e7
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2023-12-04 16:17:34 -0500

    first code commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import datetime
+
+def main():
+
+    #Display Title and Intructions
+    print("Appointment Calculator")
+    print("Please enter the date of the first appointment")
+
+    #Recieve the input
+    first_appt = input("First Appointment Day (mm/dd/yyyy): ")
+    first_appt = datetime.datetime.strptime(first_appt, "%m/%d/%Y")
+
+    #Calculate next appointment
+    test_one = datetime.timedelta(weeks = 1)
+    next_appt = first_appt + test_one
+    print("Estimated next appointment will be on " + next_appt.strftime("%m/%d/%Y"))
+
+if __name__ == "__main__":
+    main()
